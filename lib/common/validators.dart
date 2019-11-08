52b865353242f049e3ddfb279aee9811244383c7
jorgesarabia <jorge@sarabiajor.ge>
2019-11-08 17:52:13 -0300

    Se aliviana la restriccion para el password

--- lib/common/validators.dart
+++ lib/common/validators.dart
@@ -2,16 +2,19 @@ class Validators {
   static final RegExp _emailRegExp = RegExp(
     r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
   );
-  static final RegExp _passwordRegExp = RegExp(
-    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
-  );
 
   static isValidEmail(String email) {
     return _emailRegExp.hasMatch(email);
   }
 
+
+  // static final RegExp _passwordRegExp = RegExp(
+    // r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
+  // );
+
   static isValidPassword(String password) {
-    return _passwordRegExp.hasMatch(password);
+    // return _passwordRegExp.hasMatch(password);
+    return password.length > 6;
   }
 
 }
