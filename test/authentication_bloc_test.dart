e53c5f9e3bb7edf799dd73d06ab40c98daf63921
jorgesarabia <jorge@sarabiajor.ge>
2019-11-26 15:22:20 -0300

    Se agregan mas tests a auth

--- test/authentication_bloc_test.dart
+++ test/authentication_bloc_test.dart
@@ -1,17 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:lista_pacientes/User/repository/auth_repository.dart';
 import 'package:lista_pacientes/common/authentication_bloc/bloc.dart';
 import 'package:mockito/mockito.dart';
 
 class MockUserRepository extends Mock implements AuthRepository {}
+class FirebaseAuthMock extends Mock implements FirebaseAuth {}
+class FirebaseUserMock extends Mock implements FirebaseUser {}
+class GoogleSignInMock extends Mock implements GoogleSignIn {}
+class GoogleSignInAuthenticationMock extends Mock implements GoogleSignInAuthentication {}
+class GoogleSignInAccountMock extends Mock implements GoogleSignInAccount {}
 
 void main() {
   AuthenticationBloc authenticationBloc;
+
   MockUserRepository authRepository;
+  FirebaseAuthMock firebaseAuthMock;
+  FirebaseUserMock firebaseUserMock;
+  GoogleSignInMock googleSignInMock;
+  GoogleSignInAuthenticationMock googleSignInAuthenticationMock;
+  GoogleSignInAccountMock googleSignInAccountMock;
 
   setUp(() {
     authenticationBloc = AuthenticationBloc(userRepository: AuthRepository());
+    
     authRepository = MockUserRepository();
+    firebaseAuthMock = FirebaseAuthMock();
+    firebaseUserMock = FirebaseUserMock();
+    googleSignInMock = GoogleSignInMock();
+    googleSignInAuthenticationMock = GoogleSignInAuthenticationMock();
+    googleSignInAccountMock = GoogleSignInAccountMock();
   });
 
   tearDown(() {
